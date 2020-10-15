1b1d4aba586c9759cb36a676bdadc091ebe9f998
inancyillmaz <inancyillmaz@gmail.com>
2020-10-15 14:20:36 +0300

    Syntax

--- app/src/main/java/com/pokes/MainActivity.kt
+++ app/src/main/java/com/pokes/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.pokes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.pokes.Retrofit.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-lateinit var postService: PokemonService
+lateinit var Service: PokemonService
 lateinit var List: List<Pokelist>
 
 
@@ -19,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        postService = RetrofitClient.getClient().create(PokemonService::class.java)
-        var post = postService.listPost(30,30);
+        Service = RetrofitClient.getClient().create(PokemonService::class.java)
+        var post = Service.listPost(30,30);
 
         post.enqueue(object : Callback<List<Pokelist>> {
             override fun onFailure(call: Call<List<Pokelist>>, t: Throwable) {
